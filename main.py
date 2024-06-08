aa3fa9b6959fcebb3f8aad0ef8f7ebd3dfa55cc0
Tsai Cheng Peng <Canboo@users.noreply.github.com>
2024-06-09 02:02:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import functions_framework
+import os
+import sqlalchemy
+import pymysql
+from flask import Flask, jsonify
 
-def main(request):
-    return "Hello, World!"
+# 設定資料庫連接資訊
+def get_connection():
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=os.environ["DATABASE_USER"],
+            password=os.environ["DATABASE_PASSWORD"],
+            database=os.environ["DATABASE_NAME"],
+            host=os.environ["INSTANCE_HOST"]
+        )
+    )
+    return pool
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_dept():
+    connection = get_connection()
+    with connection.connect() as conn:
+        result = conn.execute("SELECT dept_name, dept_name_eng FROM tbl_dept")
+        departments = [{"dept_name": row["dept_name"], "dept_name_eng": row["dept_name_eng"]} for row in result]
+        return jsonify(departments)
+
+def hello_world(request):
+    return get_dept()
